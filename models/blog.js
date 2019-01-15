3b1ec843209202d057945296c24a1a1f5a693661
teayoung <kty7158@gmail.com>
2019-01-15 15:02:22 -0800

    for lori

--- models/blog.js
+++ models/blog.js
@@ -1,18 +1,63 @@
-var orm = require("../config/orm.js");
+module.exports = function (sequelize, DataTypes) {
+    var Post = sequelize.define("Post", {
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            varlidate: {
+                len: [1]
+            }
+        },
 
-var blog = {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
 
+        city_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            varlidate: {
+                len: [1]
+            }
+        },
+
+        state_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            varlidate: {
+                len: [1]
+            }
+        },
+
+        country_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            varlidate: {
+                len: [1]
+            }
+        },
+
+        weather_condition: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            varlidate: {
+                len: [1]
+            }
+        },
+
+        weather_input: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            varlidate: {
+                len: [1]
+            }
+        }
+
+    });
+
+
+    return Post;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = blog;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,11 @@
+var db = require("../models");
+
+module.exports = function(app) {
+
+    app.get("/api/posts", function (req, res) {
+        var query = {};
+        if (req.query.user_id) {
+            
+        }
+    })
+}
