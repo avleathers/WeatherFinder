a4ff1519c152eb45334c5e25726f14e35413f60c
Colin <beutt102@mail.chapman.edu>
2019-01-16 10:45:16 -0800

    update

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,4 +1,11 @@
-$(document).ready(function(){
-    $('.modal').modal();
-    $('.dropdown-trigger').dropdown();
- });
+// $(document).ready(function(){
+//     $('.modal').modal();
+//     $('.dropdown-trigger').dropdown();
+//  });
+
+$("input").keyup(function(){
+    var txt = $("input").val();
+    $.post("demo_ajax_gethint.asp", {suggest: txt}, function(result){
+      $("span").html(result);
+    });
+  });
