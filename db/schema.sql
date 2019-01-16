e5a1724d3fc80a7a2b56028241bda43e9d0089c2
Lori Salazar <loriatuci@gmail.com>
2019-01-15 19:48:53 -0800

    Added blog title & weather condition

--- db/schema.sql
+++ db/schema.sql
@@ -7,9 +7,11 @@ CREATE TABLE weather_inputs
 (
 	id int NOT NULL AUTO_INCREMENT,
 	user_name varchar(255) NOT NULL,
+	blog_title varchar(100) NOT NULL,
 	city_name varchar(255) NOT NULL,
 	state_name varchar(255) NOT NULL,
 	country_name varchar(255) NOT NULL,
+	weather_condition varchar(25) NOT NULL,
 	weather_input varchar(255) NOT NULL,
     ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
