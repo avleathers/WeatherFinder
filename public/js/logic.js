f9f9b598065fb3805491cacf4767dedd063a16af
Colin <beutt102@mail.chapman.edu>
2019-01-18 10:22:58 -0800

    update

--- public/js/logic.js
+++ public/js/logic.js
@@ -40,37 +40,7 @@ function clearForm(){
     $('input').not(':weatherCondition, :blogTitle, :userName, :cityName, :stateName, :countryName, :weatherInput').val('');
 };
 
-// var messageBox = document.getElementById("blogPosts");
 
 
 
-
-// function clearAndShow() {
-//     weatherInput.value = "";
-//     blogTitleInput.value = "";
-//     userNameInput.value = "";
-//     cityInput.value = "";
-//     stateInput.value = "";
-//     countryInput.value = "";
-
-//     messageBox.innerHTML = "";
-
-//     messageBox.innerHTML += "Weather Condition: " + weatherInput.value("") + "<br/>";
-//     messageBox.innerHTML += "Blog Title: " + blogTitleInput.value("") + "<br/>";
-//     messageBox.innerHTML += "User name: " + userNameInput.value("") + "<br/>";
-//     messageBox.innerHTML += "City: " + cityInput.value("") + "<br/>";
-//     messageBox.innerHTML += "State: " + stateInput.value("") + "<br/>";
-//     messageBox.innerHTML += "Country: " + countryInput.value("");
-
-// };
-
-
-    // document.write("weatherCondition");
-    // document.write("blogTitle");
-    // document.write("userName");
-    // document.write("city");
-    // document.write("state");
-    // document.write("country");
-
-    // clearAndShow();
-
+   
