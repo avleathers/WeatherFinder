b339a59c146618e97324b0ee4c5108250c0dbffe
Colin <beutt102@mail.chapman.edu>
2019-01-18 10:41:43 -0800

    update

--- public/js/logic.js
+++ public/js/logic.js
@@ -3,12 +3,6 @@ $(document).ready(function () {
     $('select').formSelect();
 });
 
-// $("input").keyup(function () {
-//     var txt = $("input").val();
-//     $.post("demo_ajax_gethint.asp", { suggest: txt }, function (result) {
-//         $("span").html(result);
-//     });
-// });
 function insert() {
     var weatherCondition = $("#weatherCondition").val();
     var blogTitle = $("#blogTitle").val();
@@ -37,7 +31,7 @@ $("#btn btn-success submit btn-lg" ).on( "click", clearForm());
 
 
 function clearForm(){
-    $('input').not('#weatherCondition, #blogTitle, #userName, #cityName, #stateName, #countryName, #weatherInput').val('');
+    $('blogForm').not('#weatherCondition, #blogTitle, #userName, #cityName, #stateName, #countryName, #weatherInput').val('');
 };
 
 
