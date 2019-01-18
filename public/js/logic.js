f77cc8bb04adfd920d0054e961546217725f297c
Colin <beutt102@mail.chapman.edu>
2019-01-18 13:49:18 -0800

    update

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,91 +1,42 @@
+//enables the dropdown menu
+
 $(document).ready(function () {
-    /* global moment */
-    var blogInputs = $("input.blogInput");
-    var blogContainer = $(".submit");
-
     $('select').formSelect();
-    $(document).on("submit", "blogPosts", postNewBlog)
+});
 
+//defines the user input values and creates an object to be passed to the server
 
-    var blogs = [];
-    getPosts();
+function insert() {
+    var weatherCondition = $("#weatherCondition").val();
+    var blogTitle = $("#blogTitle").val();
+    var userName = $("#userName").val();
+    var cityName = $("#cityName").val();
+    var stateName = $("#stateName").val();
+    var countryName = $("#countryName").val();
+    var weatherInput = $("#weatherInput").val();
 
-
-    function initializeColumn() {
-        blogContainer.empty();
-        var blogsToAdd = [];
-        for (var i = 0; i < blogs.length; i++) {
-            blogsToAdd.push(postNewBlog(blogs[i]));
-        }
-        blogContainer.prepend(blogsToAdd);
-    }
-
-    function getPosts() {
-        $.get("/api/posts", function (data) {
-            blogs = data;
-            initializeColumn();
-        });
-    }
-
-    function postNewBlog() {
-
-
-        var weatherCondition = $("#weatherCondition").val();
-        var blogTitle = $("#blogTitle").val();
-        var userName = $("#userName").val();
-        var cityName = $("#cityName").val();
-        var stateName = $("#stateName").val();
-        var countryName = $("#countryName").val();
-        var weatherInput = $("#weatherInput").val();
-
-        var data = {
-            weatherCondition,
-            blogTitle,
-            userName,
-            cityName,
-            stateName,
-            countryName,
-            weatherInput,
-        };
-        console.log(data)
-        clearForm();
-
+    var data = {
+        weatherCondition,
+        blogTitle,
+        userName,
+        cityName,
+        stateName,
+        countryName,
+        weatherInput,
     };
-    
-    //clears the form after the user has submitted the data and created the object
+    console.log(data)
+    clearForm();
 
-    function clearForm() {
-        $('#weatherCondition').val('')
-        $('#blogTitle').val('');
-        $('#userName').val('');
-        $('#cityName').val('');
-        $('#stateName').val('');
-        $('#countryName').val('');
-        $('#weatherInput').val('');
-    };
+};
 
+//clears the form after the user has submitted the data and created the object
 
-
-
-    function insertBlog(event) {}
-
-
-
-    $.post("/api/posts", data, getPosts);
-    blogInputs.val("");
-
-
-
-
-
-
-})
-
-
-
-
-
-
-
-
-
+function clearForm() {
+    $('#weatherCondition').val('')
+    $('#blogTitle').val('');
+    $('#userName').val('');
+    $('#cityName').val('');
+    $('#stateName').val('');
+    $('#countryName').val('');
+    $('#weatherInput').val('');
+};
